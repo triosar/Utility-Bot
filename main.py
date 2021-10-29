8664b3eb9437b7bf6bdc34f21144dd225f43e1a4
Triosar <78569607+triosar@users.noreply.github.com>
2021-10-29 13:54:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,100 @@
-#hey why are you looking here!!
+import requests
+import os
+from ro_py import users
+from ro_py.users import User
+import discord
+from dotenv import load_dotenv
+import keep_alive
+import random
+import time
+from humanfriendly import format_timespan
+from discord.ext import commands
+from replit import db
+import io
+import contextlib
+from discord.utils import get
+from discord import Webhook, AsyncWebhookAdapter
+import aiohttp
+import logging
+from trello import TrelloApi
+import asyncio
+from ro_py import Client
+import traceback
+import logging
 
-print("bobux dealer")
+serverlist = [] # server names
+slist = [] #server objects
+blacklist = [] # bot blacklist
+
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix='c>',intents=intents)
+
+logging.basicConfig(format='%(filename)s: %(message)s',level=logging.INFO)
+
+load_dotenv()
+DISCTOKEN = os.getenv('DISCORD_TOKEN')
+#RS = os.getenv('ROBLOSECURITY') # not needed
+#TRELLO_APP_KEY = os.getenv('TRELLO_APP_KEY') # not needed
+
+# bot function definitions
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you type! | Prefix is 'c>'!"))
+    channel = bot.get_channel(903626137001918484)
+    embedVar2 = discord.Embed(title="Bot connected to Discord",description=f'Bot successfully connected to Discord at {time.asctime()}.',color=0x0090ff)
+    embedVar2.set_footer(text="Hello world | FrogBot")
+    await channel.send(embed=embedVar2)
+
+    for server in bot.guilds:
+      serverlist.append(str(server))
+      slist.append(server)
+      #await server.leave() #activate this to leave all servers
+
+@bot.event
+async def on_message(message):
+
+  if message.author == bot.user:
+    return
+
+  if (message.webhook_id):
+    # ignore webhook posts
+    return
+
+  if message.channel.type is discord.ChannelType.private:
+    channel = bot.get_channel(903627366302109697)
+    user = str(message.author.id)
+    user = "<@"+user+">"
+
+    embedVar = discord.Embed(title="New Froggo DM", description="",color=0x0090ff)
+
+    embedVar.add_field(name="Username:", value=(user), inline=False)
+    embedVar.add_field(name="Message:",value = message.content,inline=False)
+      
+    await channel.send(embed=embedVar)
+  if str(message.author.id) not in blacklist:
+    await bot.process_commands(message)
+
+@bot.command()
+async def say(ctx,*args):
+  if str(ctx.message.author) != "puptaco#3335":
+    return
+  await ctx.send(' '.join(args))
+  await ctx.message.delete()
+
+
+@bot.command()
+async def eval(ctx, *, code):
+    if (ctx.message.author.id) != 314394344465498122:
+      await ctx.send("Denied.")
+      return
+    str_obj = io.StringIO() #Retrieves a stream of data
+    try:
+        with contextlib.redirect_stdout(str_obj):
+            exec(code)
+    except Exception as e:
+        return await ctx.send(f"```{e.__class__.__name__}: {e}```")
+    await ctx.send(f'```{str_obj.getvalue()}```')
+
+keep_alive.keep_alive()
+bot.run(DISCTOKEN)
